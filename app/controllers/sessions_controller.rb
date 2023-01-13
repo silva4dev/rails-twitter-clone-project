5668a3f2c7a7bd81e84660eaf40a09a541b758d8
silva4dev <web4dev.lucas@outlook.com>
2023-01-13 00:12:55 -0300

    Mudando as versões das gems

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,3 @@
 class SessionsController < Devise::SessionsController
-  prepend_before_filter(only: [:create, :destroy]) { request.env["devise.skip_timeout"] = true }
+  prepend_before_action(only: [:create, :destroy]) { request.env["devise.skip_timeout"] = true }
 end
